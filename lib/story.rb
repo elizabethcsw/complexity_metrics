5bb1813613dc6a659391f06b1573d29af35f7db3
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-08-21 11:30:48 +0100

    further refactor

--- lib/story.rb
+++ lib/story.rb
@@ -1,8 +1,12 @@
 class Story
   def tell
     introduction
-    if gets.downcase.start_with? "y"
-      puts "Do you like it a lot?"
+    model
+  end
+
+  def model
+    if ans_yes
+      ask_further
       yes
     else
       sad
@@ -14,16 +18,24 @@ class Story
     puts "Forever :("
   end
 
+  def ask_further
+    puts "Do you like it a lot?"
+  end
+
   def introduction
     puts "Once upon a time there was a good horse."
     puts "Do you like this horse?"
   end
 
   def yes
-    if gets.downcase.start_with? "y"
+    if ans_yes
       puts "It lived happily ever after"
     else
       puts "It lived an okay life"
     end
+  end
+
+  def ans_yes
+    gets.downcase.start_with? "y"
   end
 end
